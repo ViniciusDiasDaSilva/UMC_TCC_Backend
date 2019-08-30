026cdc916b06326cd69f8690798aa419f1b0440f
EdisonLinsFilho <edison.slins@gmail.com>
2019-08-30 20:38:56 -0300

    Firs commit

---
+++ src/main/java/com/brinquedotecaBE/brinquedotecaBE/BrinquedotecaBeApplication.java
@@ -0,0 +1,13 @@
+package com.brinquedotecaBE.brinquedotecaBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrinquedotecaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrinquedotecaBeApplication.class, args);
+	}
+
+}
